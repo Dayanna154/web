e483bce5088474a3ae35d3cf495e4d6cea6826a2
Dayanna154 <alejandraninon@gmail.com>
2025-03-12 09:46:52 -0500

    Add files via upload

---
+++ trabajo.php
@@ -0,0 +1,27 @@
+<html>
+<head></head>
+<body>
+<?php
+echo "Hola mi nombre es Dayanna Alejandra Niño Ibarra ";
+echo"<br>";
+echo" _________________________________________Tengo la edad de 20 años.  ";
+echo"<br>";
+echo"________________________________________________________________________ Vivo en Bogota  ";
+echo"<br>";
+echo " me gusta, bailar ,cantar , ver series y peliculas   ";
+echo"<br>";
+echo "_________________________________________________________ estudio idiomas";
+echo"<br>";
+echo" ____________________________________________________________________________________me gusta mucho la danza ";
+echo"<br>";
+echo" mi musica favorita es la salsa ";
+echo"<br>";
+echo" _______________________________________Mi comida favorita es el arroz con pollo ";
+echo"<br>";
+echo" ________________________________________________________________________________________ me gusta mucho viajar ";
+echo"<br>";
+echo" soy muy extrovertida ";
+ 
+?>
+</body>
+</html>
