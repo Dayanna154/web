902872576bbe7c4aa916f615b05fae947f291391
Dayanna154 <alejandraninon@gmail.com>
2025-03-12 09:38:46 -0500

    Add files via upload

---
+++ primertrabajo.php
@@ -0,0 +1,49 @@
+<html>
+<head></head>
+<body>
+<?php
+echo"Bienvendos a clase de programacion";
+echo"<br>";
+echo "el dia 14 de febrero del 2025 , aproximadamente a las 7:30 pm";
+echo"<br>";
+echo "me encontraba esperando el alimentador hacia mi casa ";
+echo"<br>";
+echo " decido sacar mi celular para mirar un mensaje ,  mi amigo se volteo y provoca que se me caiga el celular ";
+echo"<br>";
+echo " el celular cae en medio de la via , y dado que estaba lloviendo este se resbala por la alcantarilla del portal ";
+echo"<br>";
+echo " Me asome a ver si mi celular se encontraba pero la alcantarilla estaba llena de agua y basura ";
+echo"<br>";
+echo "la gente me decia que la alcantarrilla tenia conducto , por lo cual me asuste";
+echo"<br>";
+echo "sin embargo yo estaba decidida a rescatr mi celular , la gente me ayudo a levantar la tapa de la alcantarilla";
+echo"<br>";
+echo " yo estaba decidida a meterme a la alcantarilla a recuperar mi celular";
+echo"<br>";
+echo "pero los señores de transmilenio no me querian dejar entrar";
+echo"<br>";
+echo " entonces pedimos el recogedor a los señores de transmilenio para ver si la alcanrtarilla era profunda";
+echo"<br>";
+echo "yo ya estaba a punto de llorar , porque mi celular ya llevaba media hora por alla abajo y pense que se iba a dañar";
+echo"<br>";
+echo "un señor me ayuo y con el recogedor empezo a recoger la basura de la alcantarilla";
+echo"<br>";
+echo "cuando de repente saca mi celular";
+echo"<br>";
+echo "las señoras del aseo de transmilenio me dieron agua ";
+echo"<br>";
+echo "lave mi celular";
+echo"<br>";
+echo "pense que mi celular no iba a prender , pero funciono";
+echo"<br>";
+echo "le compre una empanada con gaseosa al señor que me ayudo";
+echo"<br>";
+echo "llegue a la casa y seque el celular con un secador";
+echo"<br>";
+echo "puse el celular en arroz";
+echo"<br>";
+echo "lo puse a cargar y ya ";
+?>
+</body>
+</html>
+
